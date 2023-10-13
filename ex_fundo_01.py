1ae47178308cc900aed6959c277b4f7ef0da863c
SrMorpheus <brunobarroscc@gmail.com>
2023-10-13 13:02:41 -0300

    colisao e pulo

--- ex_fundo_01.py
+++ ex_fundo_01.py
@@ -99,7 +99,7 @@ def load_map(file_path):
             elements = line.strip().split(",")
             if len(elements) == 2:
                 x, y = map(int, elements)
-                game_map[(x, y - 104)] = "."
+                game_map[(-x, y - 104)] = "."
     return game_map;
 
 # função para salvar o arquivo de mapeamento
@@ -131,15 +131,24 @@ pos_bg = (0,0)
 
 
 #======================================================================================
-#teste
+#Configuração do mapeamento
 file_path = 'mapeamento.txt'
 game_map = load_map(file_path)
+
+
+#teste do pulo 
+
+posicao_y = 232  # A posição vertical inicial do personagem (ajuste conforme necessário)
+velocidade_y = 0  # A velocidade vertical inicial
+gravidade = 0.5  # A força da gravidade que afeta a descida
+pulando = False  # Variável para rastrear se o personagem está pulando
+altura_pulo = 20  # A altura do pulo (ajuste conforme necessário)
 
 
 #movimentacao
 velocity = 40 #verificar esse velocidade
 direcao = None
-position = 0
+position = -67
 
 print( "Imagens fatiadas: ", tiles, SCREEN_WIDTH, bg_width )
 
@@ -161,18 +170,25 @@ cenario_02 = True
 pos_y = 5   # posicao da imagem do cenario
 
 id_ator = 0
-pos_ator=( position+10, 232 )
+pos_ator=( position+ 138, posicao_y )
 
-#pos_cenario = load_pos_cenario() ## mapeamento
-#print( pos_cenario )
+
+
+
+
+posicoes_x = []
+posicoes_y = []
 
 while run:
 
   screen.fill( dia )
 
+  
+
+
   clock.tick(FPS)
   if debug:
-    print(direcao, position, tiles, bg_width )
+    print("direcao", position, tiles, bg_width )
 
   if cenario_02 == True:
       #for index, img in enumerate( reversed( bg_images ) ):
@@ -180,13 +196,19 @@ while run:
 #      fundo( screen, cor_piso, bg_rect, DIM_SCREEN, pos_y )    # mesma cor do terreno
       show_persona( screen, pos_ator, id_ator, direcao )
 #      id_ator = 0
+   
+
 
   #scroll background
-  if direcao == 'esquerda':
-    position += velocity
-  if direcao == 'direita':
-    position -= velocity
+  if direcao == 'esquerda' and  (is_valid_move(game_map, position,pos_ator[1]) or is_valid_move(game_map,position + velocity,pos_ator[1])):
+        position += velocity
+  if direcao == 'direita' and  (is_valid_move(game_map, position,pos_ator[1]) or is_valid_move(game_map,position - velocity,pos_ator[1])):
+        position -= velocity
+      
+  print("Debug da posicao")
+  print(position)
 
+    
 
   #reset scroll
   if abs( position ) > bg_width:
@@ -195,13 +217,38 @@ while run:
 
   keys = pygame.key.get_pressed()  # checking pressed keys
   if keys[ pygame.K_RIGHT ]:
-      print('dIREITA.................')
-      id_ator = id_ator + 1
+            print('dIREITA.................')
+            id_ator = id_ator + 1
+
   if keys[ pygame.K_LEFT ]:
-      print('esquErdA.................')
-      id_ator = id_ator + 1
+            print('esquErdA.................')
+            id_ator = id_ator + 1
 
+  if keys[pygame.K_SPACE]:
+    if not pulando:
+      print("puladndooo")
+      pulando = True
+      velocidade_y = -altura_pulo  # Inicie o pulo com uma velocidade negativa
+      posicao_y += velocidade_y
+      velocidade_y += gravidade
+      pos_ator=( pos_ator[0], posicao_y)
+    else:
+      # Se a tecla de espaço não estiver pressionada, interrompa o pulo
+      pulando = False
+      posicao_y = 232
+      pos_ator=( pos_ator[0], posicao_y)
 
+      #testeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+  if keys[pygame.K_SPACE] and keys[pygame.K_RIGHT]:
+            if not pulando:
+                print("puladndooo para direita")
+                velocidade_y = -altura_pulo  # Inicie o pulo com uma velocidade negativa
+                posicao_y += velocidade_y
+                velocidade_y += gravidade
+                # Se a tecla de espaço não estiver pressionada, interrompa o pulo
+                pulando = False
+                pos_ator=( pos_ator[0] + 2, posicao_y)
+ 
   ## event handler
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -214,24 +261,30 @@ while run:
             pygame.quit(); sys.exit();
 
         if event.key == pygame.K_SPACE:
-            print( 'Pular' )
-
+            if not pulando:
+             print("Pular")
+             pulando = True
+             velocidade_y = -altura_pulo  # Inicie o pulo com uma velocidade negativa
+             posicao_y += velocidade_y
+             velocidade_y += gravidade
+             pos_ator=( pos_ator[0], posicao_y)
+        else:
+          # Se a tecla de espaço não estiver pressionada, interrompa o pulo
+          pulando = False
+          posicao_y = 232
+          pos_ator=( pos_ator[0], posicao_y)
         if event.key == pygame.K_LEFT:
-            direcao = 'esquerda'
-            id_ator = id_ator + 1
-            pos_ator = ( pos_ator[0]-2, pos_ator[1] )
-
+             
+             direcao = 'esquerda'
+             id_ator = id_ator + 1
+             print("INDOOooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooO")
+    
         if event.key == pygame.K_RIGHT:
             direcao = 'direita'
             id_ator = id_ator + 1
-            if(is_valid_move(game_map,pos_ator[0]+2,pos_ator[1])):
-                print("INDOOO")
-                pos_ator = ( pos_ator[0]+31, pos_ator[1] )
-                print(pos_ator[0]+2)
-            else:
-                print("nao indo")
-                pos_ator = ( pos_ator[0]+2, pos_ator[1] )
-
+            print("INDOOooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooO")
+      
+          
 
     if event.type ==pygame.KEYUP:
         if event.key == pygame.K_DOWN:
@@ -247,12 +300,14 @@ while run:
             id_ator = 0
         #id_ator = 0
 
+
   print( 'ID', id_ator )
+ 
 
   if id_ator > 6 : id_ator = 0
   if id_ator < 0 : id_ator = 6
 #print( 'ID', id_ator )
-
+  
 
 
   pygame.display.update()
